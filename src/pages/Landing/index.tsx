5ad543d3792928e2ef96991b60fd876f870f967e
Diego Miguel Pereira <corgandp@gmail.com>
2020-08-07 13:52:08 -0300

    Creating Landing page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,24 @@
+//34:41
+
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import styles from './styles';
+import landingImg from '../../assets/images/landing.png';
+
+
+function Landing() {
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} syle={styles.banner} />
+      <Text style={styles.title}>
+        Seja bem-vindo, {'\n'}
+        <Text style={styles.titleBold}>
+          O que deseja fazer?
+        </Text>
+      </Text>
+
+    </View>)
+}
+
+export default Landing;
